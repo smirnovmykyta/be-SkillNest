036a2c4c8999c2d7d82d20cc42a2e6ab44af8bf0
Smirnov <smirnov.mykyta@gmail.com>
2025-02-25 16:04:39 +0100

    added cloudinary

---
+++ middlewares/upload.js
@@ -0,0 +1,40 @@
+import multer from "multer";
+import CloudinaryStorage from "../utils/cloudinary/cloudinary.js";
+
+const ALLOWED_EXT = ['jpg', 'jpeg', 'webp', 'avif', 'png', 'mp4', 'mov', 'avi', 'mkv'];
+const ALLOWED_SIZE = 1_048_576 * 10; // 10mb
+
+const storage = new CloudinaryStorage();
+
+const fileFilter = (req, file, cb) => {
+    const fileExt = file.mimetype.split('/')[1];
+
+    if (!ALLOWED_EXT.includes(fileExt)) {
+        const err = new Error(`Wrong file type`);
+        err.statusCode = 400;
+        cb(err);
+    } else {
+        cb(null, true);
+    }
+};
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: ALLOWED_SIZE }
+}).array('files', 10);
+
+const handleFileUpload = (req, res, next) => {
+    upload(req, res, (err) => {
+        if (err) return res.status(400).json({error: err.message});
+
+        req.uploadedFiles = req.files?.map(file => ({
+            url: file.result.secure_url,
+            format: file.mimetype.split('/')[0]
+        })) || [];
+
+        next();
+    });
+};
+
+export default {upload, handleFileUpload};
