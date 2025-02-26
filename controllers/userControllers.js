9244bb70212832bdb07feb0673fb3a4f07817397
Smirnov <smirnov.mykyta@gmail.com>
2025-02-26 13:32:42 +0100

    modified userControllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,7 +4,7 @@ import ErrorResponse from "../utils/errorHandlers/ErrorResponse.js";
 
 const getAllUsers = asyncHandler(async (req, res) => {
     const users = await UserModel.find()
-        .select("_id name surname email")
+        .select("_id username email")
         .lean();
 
     if (!users) throw new ErrorResponse("User not found", 404);
@@ -19,7 +19,7 @@ const getUserById = asyncHandler(async (req, res) => {
         .select("-password")
         .lean()
         : await UserModel.findById(id)
-        .select("_id name surname email phoneNumber")
+        .select("_id username email phoneNumber")
         .lean();
 
     if (!user) throw new ErrorResponse("User not found", 404);
