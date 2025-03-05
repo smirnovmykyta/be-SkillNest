e92922a8b12037a2570851afca9b4802203e4999
zoriah <noa351@hotmail.de>
2025-03-05 21:28:23 +0100

    quick fix

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,54 +3,52 @@ import UserModel from "../models/UserModel.js";
 import ErrorResponse from "../utils/errorHandlers/ErrorResponse.js";
 
 const getAllUsers = asyncHandler(async (req, res) => {
-    const users = await UserModel.find()
-        .select("_id username email")
-        .lean();
+  const users = await UserModel.find().select("_id username email").lean();
 
-    if (!users) throw new ErrorResponse("User not found", 404);
-    res.json(users)
+  if (!users) throw new ErrorResponse("User not found", 404);
+  res.json(users);
 });
 
 const getUserById = asyncHandler(async (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    const user = await UserModel.findById(id)
-        .select("_id username email phoneNumber")
-        .lean();
+  const user = await UserModel.findById(id)
+    .select("_id username email phoneNumber")
+    .lean();
 
-    if (!user) throw new ErrorResponse("User not found", 404);
-    res.json(user)
+  if (!user) throw new ErrorResponse("User not found", 404);
+  res.json(user);
 });
 
 const getProfile = asyncHandler(async (req, res) => {
-    const id = req.user;
+  const id = req.user;
 
-    const user = await UserModel.findById(id)
-            .select("-password")
-            .lean()
+  const user = await UserModel.findById(id).select("-password").lean();
 
-
-    if (!user) throw new ErrorResponse("User not found", 404);
-    res.json(user)
+  if (!user) throw new ErrorResponse("User not found", 404);
+  res.json(user);
 });
 
-const updateUserById =  asyncHandler(async (req, res) => {
-        const id = req.user;
-        const {uploaded} = req.uploadedFiles;
-        const updateData = {...req.body};
-        if (uploaded && uploaded.length > 0) {
-        updateData.profileImg = [...req.body.profileImg, ...uploaded];
-        }
+const updateUserById = asyncHandler(async (req, res) => {
+  const id = req.user;
+  const { uploaded } = req.uploadedFiles;
+  const updateData = { ...req.body };
+  if (uploaded && uploaded.length > 0) {
+    updateData.profileImg = [...req.body.profileImg, ...uploaded];
+  }
 
-        const data = await UserModel.findByIdAndUpdate(id, updateData, {runValidators: true, new: true});
-        res.json({msg: 'Update successful', data});
-    });
+  const data = await UserModel.findByIdAndUpdate(id, updateData, {
+    runValidators: true,
+    new: true,
+  });
+  res.json({ msg: "Update successful", data });
+});
 
 const deleteUserById = asyncHandler(async (req, res) => {
-        const id = req.user;
-        const data = await UserModel.findByIdAndDelete(id);
-        if(!data) throw new ErrorResponse("User not found", 404);
-        res.json({msg: 'Successfully deleted User', data});
-    });
+  const { id } = req.params;
+  const data = await UserModel.findByIdAndDelete(id);
+  if (!data) throw new ErrorResponse("User not found", 404);
+  res.json({ msg: "Successfully deleted User", data });
+});
 
-export {getAllUsers, getUserById, updateUserById, deleteUserById, getProfile};
+export { getAllUsers, getUserById, updateUserById, deleteUserById, getProfile };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,16 +1,21 @@
-import {Router} from "express";
-import {deleteUserById, getAllUsers, getProfile, getUserById, updateUserById} from "../controllers/userControllers.js";
-import {handleFileUpload} from "../middlewares/upload.js";
-import {authenticate} from "../middlewares/authenticate.js";
+import { Router } from "express";
+import {
+  deleteUserById,
+  getAllUsers,
+  getProfile,
+  getUserById,
+  updateUserById,
+} from "../controllers/userControllers.js";
+import { handleFileUpload } from "../middlewares/upload.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const userRouter = Router();
 
-userRouter.get("/", getAllUsers)
-    .put("/", authenticate, handleFileUpload, updateUserById)
-    .delete("/", authenticate, deleteUserById);
+userRouter.get("/", getAllUsers);
 
-userRouter.get("/profile", authenticate, getProfile)
+userRouter.get("/profile", authenticate, getProfile);
 userRouter.get("/:id", getUserById);
-
+userRouter.delete("/:id", authenticate, deleteUserById);
+userRouter.put("/", authenticate, handleFileUpload, updateUserById);
 
 export default userRouter;
